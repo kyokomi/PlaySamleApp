b7e1637fa283f71b3789f917279e449288779a36
kyokomi <kyoko1220adword@gmail.com>
2014-03-11 00:27:19 +0900

    wellcome消した

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,6 +7,10 @@ import views.html.*;
 
 public class Application extends Controller {
 
+	/**
+	 * indexは「/」アクセス時に呼ばれる
+	 * @return
+	 */
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
