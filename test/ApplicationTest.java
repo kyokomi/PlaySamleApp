baedaf3470943237726995feefe95d32cfd1fec3
kyokomi <kyoko1220adword@gmail.com>
2014-03-12 01:16:52 +0900

    月姫のアイコン追加。小芝居を追加

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -35,7 +35,7 @@ public class ApplicationTest {
 
     @Test
     public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
+        Content html = views.html.index.render("Your new application is ready.", "a", "b", "c");
         assertThat(contentType(html)).isEqualTo("text/html");
         assertThat(contentAsString(html)).contains("Your new application is ready.");
     }
